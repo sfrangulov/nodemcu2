e0ef18390d68b285edd86ff26a19da04502caf08
Сергей Франгулов <sergey.frangulov@Sergejs-MacBook-Pro.local>
2016-09-21 14:52:15 +0300

    -

---
+++ _aproc.lua
@@ -0,0 +1,21 @@
+local module = {}
+
+module.indicators = {
+    sound = {
+      level = {type="level", unit = "%", range_start = 0, range_end = 100}
+    },
+    light = {
+      level = {type="level", unit = "%", range_start = 0, range_end = 100}
+    }   
+}
+
+function module.get(key, config)
+  res = {}
+  res.level = 100-100*adc.read(config.apin)/1024
+  return res
+end
+
+function module.init(key, config)
+end
+
+return module
--- config_1794771.lua
+++ config_1794771.lua
@@ -3,10 +3,12 @@ local module = {
     mqtt = {"dev_int", "dev_ext"},
     sensors = {
         dht22_1 = {module = "_dht11", type="dht22", pin = 5, pullup = true, interval = 10}
-        ,dht21_1 = {module = "_dht11", type="dht21", pin = 6, pullup = true, interval = 10}
-        ,bmp085_1 = {module = "_bmp085", sda = 2, scl = 1, interval = 10}
-        ,relay_1 = {module = "_relay", pin = 7, interval = 10, auto_off = 3}
+        --,dht21_1 = {module = "_dht11", type="dht21", pin = 6, pullup = true, interval = 10}
+        --,bmp085_1 = {module = "_bmp085", sda = 2, scl = 1, interval = 10}
+        --,relay_1 = {module = "_relay", pin = 7, interval = 10, auto_off = 3}
         --,soilhum_1 = {module = "_soilhum", pin = 3, apin = 0, interval = 10}
+        --,sound_1 = {module = "_aproc", type="sound", apin = 0, interval = 10}
+        ,light_1 = {module = "_aproc", type="light", apin = 0, interval = 10}
     },
     disabled = {}
 }
